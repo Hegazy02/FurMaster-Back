40bfaf364e109bdbe9a2879b1ecbdb1a31252b19
Hegazy <abdelrhmanhegazy02@gmail.com>
2025-05-25 02:26:53 +0300

    feat: install mongoose and connect to db

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1); // Exit process with failure code
+  });
+
 app.get("/", (req, res) => {
   res.send("Angular Node Backend");
 });
