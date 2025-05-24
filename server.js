f086a52cac3d0c302cd4fc38bf1ce567ed6a92d8
Hegazy <abdelrhmanhegazy02@gmail.com>
2025-05-25 02:23:21 +0300

    feat: install morgan

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(morgan("dev"));
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Angular Node Backend");
 });
