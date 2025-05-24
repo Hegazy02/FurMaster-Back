839c549ebab11d238ced3a23cf9b435dad73b9e3
Hegazy02 <120028585+Hegazy02@users.noreply.github.com>
2025-05-25 02:35:47 +0300

    Setup backend foundation with Express, dotenv, Morgan, Mongoose, and error handling
    * feat: install express
    
    * feat: install dotenv
    
    * feat: install morgan
    
    * feat: install mongoose and connect to db
    
    * feat: create appError handler
    
    * feat: create Global error handler

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
+const port = process.env.PORT || 3000;
+const AppError = require("./utils/appError.js");
+app.use(morgan("dev"));
 
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1); // Exit process with failure code
+  });
+
+app.get("/", (req, res) => {
+  res.send("Angular Node Backend");
+});
+
+
+app.use((err, req, res, next) => {
+  console.error("Global error handler:", err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    status: "failed",
+    statusCode,
+    message: err.message || "Something went wrong",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
