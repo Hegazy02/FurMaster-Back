603a09ea248f2fb3ca2e29aa1dce1b981ec9cbde
Hegazy <abdelrhmanhegazy02@gmail.com>
2025-05-26 00:17:25 +0300

    feat:create paymentMethods http methods,routes,controllers and validator

--- models/user.js
+++ models/user.js
@@ -1,21 +1,16 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const UsersSchema = new Schema(
-  {
-   
-    email: { type: String, required: true, unique: true },
-    firstName: { type: String, required: true},
-    lastName:{ type: String, required: true},
-    gender:{type: Number,required: true},
-    image:{ type: String},
-    phonenumber:{type:String,required: true},
-    city:{type:String,required: true},
-    street:{type:String,required: true},
-    address:{type:String,required: true},
-  
-  },
- 
-);
+const UsersSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  gender: { type: Number, required: true },
+  image: { type: String },
+  phonenumber: { type: String, required: true },
+  city: { type: String, required: true },
+  street: { type: String, required: true },
+  address: { type: String, required: true },
+});
 
 module.exports = mongoose.model("User", UsersSchema);
