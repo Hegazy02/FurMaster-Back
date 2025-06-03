b98d651fddb14d824cdc37dbc80e9b57db5cfb81
Hegazy <abdelrhmanhegazy02@gmail.com>
2025-06-03 10:28:14 +0300

    feat(admin-users): create get users and update user by admin

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const userController = require("../controllers/user.controller");
 const upload = require("../middlewares/uploadCloudinary");
-const currentUserPrefex="/me"
+const currentUserPrefex="users/me"
 router.get(currentUserPrefex, userController.getMe);
 router.patch(currentUserPrefex, upload.single("image"), userController.updateMe);
+//admin
+router.get("/admin/users", userController.getUsers);
+router.patch("/admin/users/:id", userController.updateUser);
 module.exports = router;
