449b2c3a25159d8d969e1664899c18c99c151a7c
Hegazy <abdelrhmanhegazy02@gmail.com>
2025-06-20 00:15:28 +0300

    fix: add counter model

--- models/order.js
+++ models/order.js
@@ -1,37 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
   sessionId: String,
   customerEmail: String,
   amountTotal: Number,
   currency: String,
-  status: String, 
+  status: String,
   cardLast4: String,
   cardBrand: String,
   isPaid: String,
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    ref: "User",
+    required: true,
   },
-   products: [
+  products: [
     {
       name: String,
       quantity: Number,
       unitPrice: Number,
-      totalPrice: Number,    
-       image:String,     
+      totalPrice: Number,
+      image: String,
 
-          productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-    variantId: { type: mongoose.Schema.Types.ObjectId }, 
-
-
-    }
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      variantId: { type: mongoose.Schema.Types.ObjectId },
+    },
   ],
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Order', orderSchema);
+module.exports = mongoose.model("Order", orderSchema);
