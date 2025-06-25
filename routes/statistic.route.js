94557371192a21ac6cd4c30212e29f93bb025be9
Hegazy02 <120028585+Hegazy02@users.noreply.github.com>
2025-06-25 13:42:26 +0300

    feat(dashboard): get best-selling products (#66)
    * feat:(admin-dashboard): create customer gender statistics
    
    * feat(admin-dashboard): get total orders statistics
    
    * fix: fix importing order controller
    
    * feat(dashboard): get best selling products

--- routes/statistic.route.js
+++ routes/statistic.route.js
@@ -5,8 +5,10 @@ const {
   getCustomerGenderStatistics,
   getTotalOrdersStatistics,
   getTotalOrdersAmountStatistics,
+  getBestSellingProducts,
 } = require("../controllers/statistic.controller");
 router.get("/customerGenderStatistics", getCustomerGenderStatistics);
 router.get("/totalOrdersStatistics", getTotalOrdersStatistics);
 router.get("/totalOrdersAmountStatistics", getTotalOrdersAmountStatistics);
+router.get("/bestSellingProducts", getBestSellingProducts);
 module.exports = router;
